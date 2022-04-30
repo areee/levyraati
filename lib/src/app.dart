e02abc9553519f204496307ea5c465a6885bc4ba
areee <10089872+areee@users.noreply.github.com>
2022-04-30 09:59:06 +0300

    Don't show debug mode banner
    - Updated app.dart

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -26,6 +26,7 @@ class MyApp extends StatelessWidget {
       animation: settingsController,
       builder: (BuildContext context, Widget? child) {
         return MaterialApp(
+          debugShowCheckedModeBanner: false,
           // Providing a restorationScopeId allows the Navigator built by the
           // MaterialApp to restore the navigation stack when a user leaves and
           // returns to the app after it has been killed while running in the
